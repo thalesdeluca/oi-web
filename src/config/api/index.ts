d49eef078a17788de8265b02b59351d66531c356
Thales de Luca <delucathales@gmail.com>
2020-10-22 03:19:41 -0300

    fix: change url

--- src/config/api/index.ts
+++ src/config/api/index.ts
@@ -1,7 +1,7 @@
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3333"
-})
+  baseURL: "http://68.183.121.47/deliverie-front",
+});
 
-export default api
+export default api;
