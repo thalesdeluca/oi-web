bfdc56f7c60454fd8cb8e967094bf15237bbcfbb
Thales de Luca <delucathales@gmail.com>
2020-10-23 00:16:22 -0300

    fix: url api

--- src/config/api/index.ts
+++ src/config/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://68.183.121.47/deliverie-front",
+  baseURL: "http://159.89.48.179/deliverie/",
 });
 
 export default api;
